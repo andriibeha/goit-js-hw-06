e3ae70d7bd5ea0670e7a4308ea6fcfddb2ca6162
andriibeha <beha.andrii@gmail.com>
2022-04-11 23:38:01 +0300

    js-2
    Done js-2

--- js/task-02.js
+++ js/task-02.js
@@ -7,3 +7,15 @@ const ingredients = [
   'Condiments',
 ];
 
+
+const ingredientsItemCreate = ingredients.map(ingredient => {
+  const ingredientsItemRef = document.createElement('li')
+  ingredientsItemRef.textContent = ingredient
+  ingredientsItemRef.classList.add('item')
+
+  return ingredientsItemRef;
+});
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+ingredientsListRef.append(...ingredientsItemCreate)
