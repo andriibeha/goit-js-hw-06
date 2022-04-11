19c7b3914c931b62578ffe61736bd200b76bd4ee
andriibeha <beha.andrii@gmail.com>
2022-04-11 23:21:08 +0300

    delete description
    delete description for js-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,22 +1,3 @@
-/* Напиши скрипт, який:
-
-Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-
-Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль
-текст заголовку елемента(тегу < h2 >) і кількість елементів в категорії(усіх < li >, вкладених в нього).
-
-ВИВЕСТИ:
-Number of categories: 3
-
-Category: Animals
-Elements: 4
-
-Category: Products
-Elements: 3
-
-Category: Technologies
-Elements: 5*/
-
 const categoriesListRef = document.querySelector('#categories');
 console.log('Number of categories: ', categoriesListRef.children.length);
 
