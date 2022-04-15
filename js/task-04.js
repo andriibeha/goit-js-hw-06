cc8fe9c02191ec3b1e0e09d5ee2e7d8c36add30e
andriibeha <beha.andrii@gmail.com>
2022-04-15 12:37:53 +0300

    fixed js-4
    delete coment

--- js/task-04.js
+++ js/task-04.js
@@ -1,9 +1,3 @@
-/* Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
-
-Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
-
-Оновлюй інтерфейс новим значенням змінної counterValue. */
-
 const refs = {
     BtnDecrement : document.querySelector('[data-action="decrement"]'),
     counter: document.querySelector('#value'),
@@ -23,7 +17,7 @@ function onBtnDecrement() {
 
 function onBtnIncrement() { 
     counterValue = counterValue + 1;
-    
+
     refs.counter.innerHTML = counterValue;
 }
 
